e79c9fb75986c5493fff96836405cd7fa17f9b35
Abdallah Usama <abdallah.elhanafy@outlook.com>
2024-01-18 13:39:30 +0200

    added weather for 2 days

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let weatherState = document.getElementById('weatherState')
 let humidity = document.getElementById('humidity')
 let windSpeed = document.getElementById('windSpeed')
 let cityInput = document.getElementById('cityNameInput')
-
+let twoDaysWeather = document.getElementById('twoDaysWeather')
 
 cityInput.value = ``
 
@@ -19,7 +19,10 @@ cityInput.addEventListener('keyup', function () {
         getWeather(cityQuery)
     }
 
+
 })
+
+
 
 
 async function getWeather (query){
@@ -27,22 +30,45 @@ async function getWeather (query){
      await  fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${query}&days=3&aqi=no&alerts=no`)
     let data = await response.json()
     console.log(data)
-    weatherImg.src = data.current.condition.icon
-    cityName.innerHTML = data.location.name
-    temperature.innerHTML = `${data.current.temp_c} <sup> &deg;C </sup>`
-   weatherState.innerHTML = data.current.condition.text
-    humidity.innerHTML = `${data.current.humidity}%`
-    windSpeed.innerHTML = `${data.current.gust_kph} Km/h`
+    weatherImg.src = data.current.condition.icon;
+    cityName.innerHTML = data.location.name;
+    if (data.location.name.length >= 10) {
+        cityName.classList.add('fs-4')
+    } else {
+        cityName.classList.remove('fs-4')
+    }
+    temperature.innerHTML = `${data.current.temp_c} <sup> &deg;C </sup>`;
+   weatherState.innerHTML = data.current.condition.text;
+    humidity.innerHTML = `${data.current.humidity}%`;
+    windSpeed.innerHTML = `${data.current.gust_kph} Km/h`;
+
+    let weatherBox = ``
+    for (let i =1; i<data.forecast.forecastday.length; i++) {
+        weatherBox += `
+        <div class="d-flex text-center justify-content-center align-items-center flex-column p-5">
+        <img src='${data.forecast.forecastday[i].day.condition.icon}'>
+        <h1 >${data.forecast.forecastday[i].day.avgtemp_c } <sup> &deg;C </sup></h1>
+        <h5 class="fw-lighter" >${data.forecast.forecastday[i].day.condition.text}</h5>
+        <h6 >${data.forecast.forecastday[i].date}</h6>
+        
+      
+        </div>
+
+        `
+
+    }
+    twoDaysWeather.innerHTML = weatherBox;
 }
 
 
 async function getLocationWeather () {
-    if (navigator.geolocation){
+
         let result =  await fetch('https://ipinfo.io/197.54.7.187?token=a117ca4fe8503a')
         let data = await result.json()
         console.log(data)
         getWeather(data.city)
-    }
+        $('#mainBox').animate({opacity:1},1000)
+        console.log('done')
 
 
 
